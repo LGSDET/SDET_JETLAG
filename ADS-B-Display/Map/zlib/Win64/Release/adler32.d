ec1b35d1ad186c76c35db41038fea177ba8a8822
신명수/선임연구원/키친솔루션SW개발2Project <myeongsu.shin@lge.com>
2025-05-27 13:09:00 -0400

    Register Project

---
+++ ADS-B-Display/Map/zlib/Win64/Release/adler32.d
@@ -0,0 +1,3 @@
+.\Win64\Release\adler32.o: adler32.c \
+  C:\Users\USER\Documents\LG\ADS-B-Display\Map\zlib/zlib.h \
+  C:\Users\USER\Documents\LG\ADS-B-Display\Map\zlib/zconf.h
---
+++ ADS-B-Display/Map/zlib/Win64/Release/uncompr.d
@@ -0,0 +1,3 @@
+.\Win64\Release\uncompr.o: uncompr.c \
+  C:\Users\USER\Documents\LG\ADS-B-Display\Map\zlib/zlib.h \
+  C:\Users\USER\Documents\LG\ADS-B-Display\Map\zlib/zconf.h
